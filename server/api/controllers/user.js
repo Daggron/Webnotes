c6dbb25a479f9a4172c50b95991845a52629c28c
Daggron <returnofking04@gmail.com>
2020-02-07 23:45:54 +0530

    Made the login form

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req,res)=>{
     if(!req.body.email){
@@ -27,7 +28,11 @@ exports.register = async (req,res)=>{
                 newUser.save().then(()=>{
                     res.status(200).json({
                         success : true,
-                        message : 'You have registered successfully'
+                        message : `You have successfully registered as ${newUser.name}`,
+                        user : {
+                            name : newUser.name,
+                            email : newUser.email
+                        }
                     })
                 })
             }else{
@@ -42,6 +47,48 @@ exports.register = async (req,res)=>{
             res.send(500).json({
                 success : false,
                 message : 'Error while Posting data to server'
+            })
+        }
+    }
+}
+
+
+exports.login = async (req,res)=>{
+    if(!req.body.email){
+        return res.json({
+            success : false,
+            message : "Please Enter a valid email"
+        })
+    }else{
+        try{
+
+            let user = await User.findOne({email : req.body.email}).select('_id name email password').exec();
+
+            if(!user){
+                res.json({
+                    success : false,
+                    message : "User does not exist"
+                })
+            }
+            else{
+                let match = await bcrypt.compare(req.body.password , user.password);
+                if(match){
+                    const token = await jwt.sign({id : user._id , name : user.name , email : user.email} , 'Luke Skywalker' , {algorithm : "HS256"})
+                    res.json({
+                        success : true,
+                        message : `Login Successfull! Welcome ${user.name}`,
+                        token : token,
+                        user : {name : user.name , email : user.email}
+                    })
+                }
+            }
+
+
+        }catch(err){
+            console.log(err);
+            res.json({
+                success : false,
+                message : "Error while Posting Data to server"
             })
         }
     }
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -3,4 +3,6 @@ const usercontroller = require('../controllers/user');
 
 router.route('/register').post(usercontroller.register);
 
+router.route('/login').post(usercontroller.login);
+
 module.exports = router;
