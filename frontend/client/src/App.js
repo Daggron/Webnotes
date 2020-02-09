4ec43401140ccf3cbb7ad95c2dc82e70a71587ea
Daggron <returnofking04@gmail.com>
2020-02-09 20:41:00 +0530

    Added Editor for writing notes

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter , Route , Switch } from 'react-router-dom';
 import './App.css';
+import importedComponent from 'react-imported-component'
+import loading from './loading';
+
+const Page = importedComponent(()=>import('./page'),{
+  LoadingComponent : loading
+})
+
+const Editor = importedComponent(()=>import('./components/editor'),{
+  LoadingComponent : loading
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Editor} />
+      </Switch>
+    </BrowserRouter>
+   </React.Fragment>
   );
 }
 
---
+++ frontend/client/src/components/editor.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+
+
+export default function Editor() {
+
+    const modules = {
+        toolbar : toolbarOptions
+    }
+
+    return (
+        <React.Fragment>
+           <ReactQuill modules={modules} style={{height : "50vh" , width : "90vw" , margin : "auto"}} />
+        </React.Fragment>
+    )
+}
